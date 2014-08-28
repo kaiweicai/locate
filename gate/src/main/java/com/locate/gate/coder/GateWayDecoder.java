344e8566444927f1751f166bf129e824dbfa8560
kaiweicai <kaiweicai@163.com>
2014-08-28 11:08:37 +0800

    gate first commit

---
+++ gate/src/main/java/com/locate/gate/coder/GateWayDecoder.java
@@ -0,0 +1,77 @@
+package com.locate.gate.coder;
+
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.frame.FrameDecoder;
+
+import com.locate.common.model.LocateUnionMessage;
+
+@Deprecated
+public class GateWayDecoder extends FrameDecoder {
+	
+	Logger logger = Logger.getLogger(GateWayDecoder.class);
+	
+	private final ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+	static int MSG_HEADER_LEN = 13;// length of meessage type and data's length
+	@Override
+	protected Object decode(ChannelHandlerContext ctx, Channel channel, ChannelBuffer channelBuffer) throws Exception {
+//		ChannelBuffer buffer = ChannelBuffers.dynamicBuffer();
+		if (channelBuffer.readableBytes() < 13) {
+	        // The length field was not received yet - return null.
+	        // This method will be invoked again when more packets are
+	        // received and appended to the buffer.
+			logger.error("read bytes less than 13");
+			return null;
+		}
+		
+		
+		channelBuffer.markReaderIndex();
+		int sequenceNo = channelBuffer.readInt();
+		logger.info("-----------readerIndex is --------"+channelBuffer.readerIndex());
+		byte msgType = channelBuffer.readByte();
+		logger.info("-----------readerIndex is --------"+channelBuffer.readerIndex());
+		int errorCode = channelBuffer.readInt();
+		logger.info("-----------readerIndex is --------"+channelBuffer.readerIndex());
+		int msgLength = channelBuffer.readInt();
+		logger.info("-----------readerIndex is --------"+channelBuffer.readerIndex());
+		logger.info("-----------sequenceNo is --------"+sequenceNo);
+		logger.info("-----------msgType is --------"+msgType);
+		logger.info("-----------errorCode is --------"+errorCode);
+		logger.info("-----------msgLength is --------"+msgLength);
+		if(msgLength>20000){
+			logger.error("mesge lenght too big, mesg length is "+msgLength);
+			logger.debug(msgLength);
+		}
+		if (channelBuffer.readableBytes() < msgLength) {
+			channelBuffer.resetReaderIndex();
+			return null;
+		}
+		
+		if (channelBuffer.readable()) {
+			buffer.writeBytes(channelBuffer,msgLength);
+//			channelBuffer.readBytes(buffer, channelBuffer.readableBytes());
+		}
+		
+		String content="";
+		try{
+			content = new String( buffer.readBytes(msgLength).array(),"UTF-8");
+		}catch(Exception e){
+			logger.error("",e.getCause());
+		}
+		Document doc=null;
+		try{
+			doc = DocumentHelper.parseText(content);
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		LocateUnionMessage message = new LocateUnionMessage();
+		buffer.clear();
+		return message;
+	}
+
+}
---
+++ gate/src/main/java/com/locate/gate/hanlder/AdapterHandler.java
@@ -0,0 +1,50 @@
+package com.locate.gate.hanlder;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+
+import net.sf.json.JSON;
+import net.sf.json.JSONObject;
+
+import org.apache.log4j.Logger;
+import org.dom4j.io.DocumentResult;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.handler.codec.oneone.OneToOneEncoder;
+import org.springframework.stereotype.Service;
+
+import com.locate.common.model.LocateUnionMessage;
+import com.locate.common.utils.SystemProperties;
+
+@Service
+public class AdapterHandler extends OneToOneEncoder {
+	Logger logger = Logger.getLogger(getClass());
+	private static String defaultEncode = SystemProperties.getProperties(SystemProperties.DEFAULT_ENCODE);
+	
+	@Override
+	protected Object encode(ChannelHandlerContext ctx, Channel channel, Object msg) throws Exception {
+		
+		LocateUnionMessage message = (LocateUnionMessage) msg;
+		byte[] content = null;
+		switch(defaultEncode){
+			case "JSON":
+				JSON jsonObject = JSONObject.fromObject(message);
+				content = jsonObject.toString().getBytes("UTF-8");
+				break;
+			case "XML":
+				JAXBContext context = JAXBContext.newInstance(LocateUnionMessage.class);
+				Marshaller marShaller = context.createMarshaller();
+				DocumentResult node = new DocumentResult();
+				marShaller.marshal(message, node);
+				content = node.getDocument().asXML().getBytes("UTF-8");
+				break;
+			default:
+				
+		}
+		ChannelBuffer buffer = ChannelBuffers.buffer(content.length);
+		buffer.writeBytes(content);
+		return buffer;
+	}
+}
